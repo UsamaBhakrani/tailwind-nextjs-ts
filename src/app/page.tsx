035f6ea9913ec0e3a3d60c107faa0eea1ec52cb5
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-16 17:31:41 +0500

    typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,76 @@
 export default function HomePage() {
   return (
-    <div className="flex align-center flex-col max-w-max mx-auto mt-6 border rounded shadow-lg p-5"></div>
+    <div className="container mx-auto">
+      <article>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure minima
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolorem
+          incidunt corporis porro, inventore aliquid totam. Nobis vel, incidunt
+          consequatur molestiae dolore ratione eveniet optio reiciendis maiores
+          eius repudiandae? Obcaecati quis iure veniam praesentium, cupiditate
+          id nisi harum expedita provident vel, rerum iusto itaque aliquid omnis
+          quas distinctio illo facere.
+        </p>
+        <div className="bg-slate-200 m-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 mt-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 mb-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 mx-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 my-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 mt-[50px] flex justify-center">
+          Hello World
+        </div>
+
+        <div className="bg-slate-200 p-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 pt-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 pb-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 px-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 py-2 flex justify-center">Hello World</div>
+        <div className="bg-slate-200 pt-[50px] flex justify-center">
+          Hello World
+        </div>
+        <div className="flex mt-24 space-x-24">
+          <div>item 1</div>
+          <div>item 2</div>
+          <div>item 3</div>
+          <div>item 4</div>
+          <div>item 5</div>
+          <div>item 6</div>
+          <div>item 7</div>
+        </div>
+        <div className="flex flex-col mt-24 space-y-24">
+          <div>item 1</div>
+          <div>item 2</div>
+          <div>item 3</div>
+          <div>item 4</div>
+          <div>item 5</div>
+          <div>item 6</div>
+          <div>item 7</div>
+        </div>
+        <div className="font-sans">Tailwind is Awesome</div>
+        <div className="font-serif">Tailwind is Awesome</div>
+        <div className="font-mono">Tailwind is Awesome</div>
+        <div className="font-sans">Tailwind is Awesome</div>
+        <div className="font-sans text-xs">Tailwind is Awesome</div>
+        <div className="font-serif text-xl">Tailwind is Awesome</div>
+        <div className="font-mono text-2xl">Tailwind is Awesome</div>
+        <div className="font-sans text-9xl">Tailwind is Awesome</div>
+        <div className="tracking-tight">Tailwind is Awesome</div>
+        <div className="tracking-widest">Tailwind is Awesome</div>
+        <div className="text-left">Tailwind is Awesome</div>
+        <div className="text-right">Tailwind is Awesome</div>
+        <div className="text-center">Tailwind is Awesome</div>
+        <div className="underline decoration-4">Tailwind</div>
+        <p className="uppercase">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis ducimus
+          culpa a necessitatibus quam fugit. Enim consequatur est consequuntur
+          quas, ducimus nihil animi aut quidem quia debitis natus! Et numquam
+          placeat fugiat recusandae, corrupti molestiae rerum magnam nostrum,
+          neque aut accusamus vitae nulla voluptatum omnis, voluptatem amet
+          consequuntur illo quam!
+        </p>
+      </article>
+    </div>
   );
 }
