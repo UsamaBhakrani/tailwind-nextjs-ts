c1e2517ddc3ccc27fd4a7e2346d1122defbf4eb1
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-23 17:06:10 +0500

    Transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-blue-800">
+      <body >
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,22 @@
 export default function HomePage() {
   return (
-    <div className="container mx-auto">
-      <div>
-        {/* Columns */}
-        {/* <div className="columns-2 gap-10 md:columns-4 text-cyan-400">
-          <div className="w-full h-10">Hello</div>
-          <div className="w-full h-10">Jello</div>
-          <div className="w-full h-10">Mello</div>
-          <div className="w-full h-10">Pello</div>
-        </div> */}
+    <>
+      <div className="flex justify-evenly align-middle p-5 m-5">
+        <button className="px-3 py-2 text-sm text-white bg-blue-500 rounded-lg hover:bg-black transition duration-1000">
+          Submit
+        </button>
+        <button className="px-3 py-2 text-sm text-white bg-blue-500 rounded-lg hover:opacity-20 transition duration-1000">
+          Submit
+        </button>
       </div>
-      {/* <div className="columns-xs gap-10 text-slate-400">
-        <div className="w-full h-10">Hello</div>
-        <div className="w-full h-10">Jello</div>
-        <div className="w-full h-10">Mello</div>
-        <div className="w-full h-10">Pello</div>
-      </div> */}
-      {/* FlexBox */}
-      {/* <div className="flex h-96 w-full bg-gray-100 items-center flex-wrap justify-between flex-col md:flex-row text-purple-600">
-        <div className="p-10 border border-blue-300 bg-blue-100">1</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">2</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">3</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">4</div>
+      <div className="flex justify-evenly align-middle p-5 m-5">
+        <button className="px-3 py-2 text-sm text-white bg-blue-500 rounded-lg hover:bg-black transition duration-1000">
+          Submit
+        </button>
+        <button className="px-3 py-2 text-sm text-white bg-blue-500 rounded-lg hover:scale-150 duration-500">
+          Submit
+        </button>
       </div>
-      <div className="flex flex-col h-96 w-full bg-gray-100 items-center flex-wrap justify-between md:flex-row text-purple-600">
-        <div className="p-10 border border-blue-300 bg-blue-100">1</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">2</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">3</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">4</div>
-      </div> */}
-
-      {/* <div className="flex w-full bg-gray-100">
-        <div className="w-64 p-10 border border-blue-300 bg-blue-100">1</div>
-        <div className="flex-none w-64 p-10 border border-blue-300 bg-blue-100">
-          2
-        </div>
-        <div className="flex-auto w-64 p-10 border border-blue-300 bg-blue-100">
-          3
-        </div>
-        <div className="w-64 p-10 border border-blue-300 bg-blue-100">4</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">5</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">6</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">7</div>
-        <div className="p-10 border border-blue-300 bg-blue-100">8</div>
-      </div>
-      <div className="flex text-center">
-        <div className="flex-1 bg-blue-500 border border-cyan-400">Hello</div>
-        <div className="flex-1 bg-blue-500 border border-cyan-400">Hello</div>
-        <div className="flex-1 bg-blue-500 border border-cyan-400">Hello</div>
-      </div> */}
-      
-    </div>
+    </>
   );
 }
